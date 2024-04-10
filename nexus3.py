8af0db346b75ae21e285a64293273911bf4cf7a2
Overo3 <98050377+Overo3@users.noreply.github.com>
2024-04-10 14:31:34 -0400

    Update nexus3.py
    Added youtube load function

--- nexus3.py
+++ nexus3.py
@@ -1,4 +1,5 @@
 import discord
+from pytube import YouTube
 import os
 
 TOKEN = os.environ["DISCORD_TOKEN"]
@@ -24,5 +25,12 @@ async def on_message(message):
             await message.channel.send(eventsDB[message.content[12:len(message.content)]])
         except KeyError:
             await message.channel.send("EventsDB entry not found.")
-
+    if message.content.startswith('$nexus mp3'):
+        try:
+            vidLink = message.content[11:len(message.content)]
+            yt = YouTube(vidLink)
+            stream = yt.streams.filter(only_audio=True).first()
+            stream.download(filename=f'mp3files/"{yt.title}".mp3')
+            with open(f'mp3files/"{yt.title}".mp3') as file:
+                await message.channel.send(file=file)
 client.run(TOKEN)
